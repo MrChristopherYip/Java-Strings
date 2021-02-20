685340e49d43fbac768c7042025f6a1bf039b217
MrChristopherYip <47013268+MrChristopherYip@users.noreply.github.com>
2021-02-20 06:00:28 -0800

    Added Palindrome class

---
+++ Palindrome.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter word: ");
+        String s = input.next();
+
+        boolean isPal = isPalindrome(s);
+        System.out.println("\"" + s + "\"" + " is " + (isPal ? "" : "not ") + "a palindrome.");
+    }
+
+    static boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        return s.equalsIgnoreCase(sb.toString());
+    }
+}
